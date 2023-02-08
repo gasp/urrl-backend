3a4fb3f3f27a42335c7f2da9a9a08ebb40552aca
gaspard <gaspard@gmail.com>
2023-02-08 13:51:50 +0100

    fix: b62 reverse compat

--- src/lib/shortUrl.test.ts
+++ src/lib/shortUrl.test.ts
@@ -2,17 +2,21 @@ import { intToShortUrl, shortUrlToInt } from './shortUrl'
 
 describe('shortUrl generator', () => {
   it('generates unique strings', () => {
+    const zero = 0
     const under62 = 42
     const over62 = 64999
+    expect(intToShortUrl(zero)).toBe('0')
     expect(intToShortUrl(under62)).toBe('g')
-    expect(intToShortUrl(over62)).toBe('NuG')
+    expect(intToShortUrl(over62)).toBe('GuN')
   })
   it('looks what dedicated urls look like', () => {
-    expect(shortUrlToInt('graphql')).toBe(2434709206011)
-    expect(shortUrlToInt('health')).toBe(39993529145)
+    expect(shortUrlToInt('B1')).toBe(683)
+    expect(shortUrlToInt('Xo9')).toBe(129961)
   })
   it('checks backward compat', () => {
     const int = 42
+    const short = 'g1'
     expect(shortUrlToInt(intToShortUrl(int))).toBe(int)
+    expect(intToShortUrl(shortUrlToInt(short))).toBe(short)
   })
 })
--- src/lib/shortUrl.ts
+++ src/lib/shortUrl.ts
@@ -2,12 +2,15 @@
 const BASE62 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 export const intToShortUrl = (id: number): string => {
+  if (id === 0) {
+    return BASE62[0]
+  }
   let shortUrl = ''
-  while (id) {
+  while (id > 0) {
     shortUrl += BASE62[id % 62]
     id = Math.floor(id / 62)
   }
-  return shortUrl
+  return shortUrl.split('').reverse().join('')
 }
 export const shortUrlToInt = (shortUrl: string): number => {
   let id = 0
